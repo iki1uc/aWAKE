2cda024663b822810c49e27b999fac93745b247a
iki1uc <entertrainer@web.de>
2026-07-01 21:05:22 +0200

    Add SLAVE object with methods for operations

--- slave.js
+++ slave.js
@@ -1,1 +1,30 @@
+export const SLAVE = {
+  id: "SLAVE",
+  mode: "WORK",
+  tick: 0,
+
+  scan(x) {
+    return `SCAN(${x})`;
+  },
+
+  carry(x) {
+    return `CARRY(${x})`;
+  },
+
+  push(x) {
+    return `PUSH(${x})`;
+  },
+
+  run(input) {
+    this.tick++;
+
+    return {
+      id: this.id,
+      tick: this.tick,
+      scan: this.scan(input),
+      carry: this.carry(input),
+      push: this.push(input)
+    };
+  }
+};
 
