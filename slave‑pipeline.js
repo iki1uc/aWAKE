5f0ab651f96aae3164eb532036aca07c5b9dd018
iki1uc <entertrainer@web.de>
2026-07-01 21:05:56 +0200

    Update slave‑pipeline.js

--- slave‑pipeline.js
+++ slave‑pipeline.js
@@ -1,1 +1,13 @@
+import { SLAVE } from "./slave.js";
 
+export function SLAVE_PIPELINE(masterBlock) {
+
+  const input = masterBlock.wake.id + ":" + masterBlock.wake.state;
+
+  const result = SLAVE.run(input);
+
+  return {
+    master: masterBlock,
+    slave: result
+  };
+}
