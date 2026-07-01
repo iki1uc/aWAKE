25dc45347d38c4f02cfc9132cf99d08dde2a25e5
iki1uc <entertrainer@web.de>
2026-07-01 21:15:26 +0200

    Update wurmloch-core.js

--- wurmloch-core.js
+++ wurmloch-core.js
@@ -1,1 +1,25 @@
+export const WURMLOCH_CORE = {
+  id: "WURMLOCH_CORE",
+  state: "ACTIVE",
+  depth: 0,
+
+  fold(space) {
+    return `FOLD(${space})`;
+  },
+
+  tunnel(space) {
+    return `TUNNEL(${space})`;
+  },
+
+  run(space) {
+    this.depth++;
+
+    return {
+      id: this.id,
+      depth: this.depth,
+      fold: this.fold(space),
+      tunnel: this.tunnel(space)
+    };
+  }
+};
 
