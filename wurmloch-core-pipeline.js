26fa40660b589d080427bbc880cdea7f89db5809
iki1uc <entertrainer@web.de>
2026-07-01 21:16:02 +0200

    Add WURMLOCH_CORE_PIPE function for galaxy warp

--- wurmloch-core-pipeline.js
+++ wurmloch-core-pipeline.js
@@ -1,1 +1,17 @@
+import { START_GALAXY_WARP } from "./START_GALAXY_WARP.js";
+import { WURMLOCH_CORE } from "./wurmloch-core.js";
+
+export function WURMLOCH_CORE_PIPE() {
+  const warpBlock = START_GALAXY_WARP();
+
+  const space =
+    warpBlock.warp.worm.gate + ":" + warpBlock.warp.worm.warp;
+
+  const coreResult = WURMLOCH_CORE.run(space);
+
+  return {
+    warp: warpBlock,
+    core: coreResult
+  };
+}
 
