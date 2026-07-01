db051127bb441c23fa951ba8ba052ce027d0129d
iki1uc <entertrainer@web.de>
2026-07-01 21:07:06 +0200

    Update START_SLAVE.js

--- START_SLAVE.js
+++ START_SLAVE.js
@@ -1,1 +1,8 @@
+import { START } from "./start.js";
+import { SLAVE_PIPELINE } from "./slave-pipeline.js";
+
+export function START_SLAVE() {
+  const masterBlock = START();
+  return SLAVE_PIPELINE(masterBlock);
+}
 
