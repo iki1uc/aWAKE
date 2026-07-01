424c559629787bdb3120fafd92a62b2496adff53
iki1uc <entertrainer@web.de>
2026-07-01 21:12:34 +0200

    Implement GALAXY_WARP_PIPE function

--- galaxy‑warp‑pipeline.js
+++ galaxy‑warp‑pipeline.js
@@ -1,1 +1,22 @@
+import { GALAXY_SLAVE } from "./GALAXY_SLAVE.js";
+import { GALAXY_WARP } from "./GALAXY_WARP.js";
+
+export function GALAXY_WARP_PIPE(masterBlock) {
+
+  const slaveResult = GALAXY_SLAVE.run(masterBlock.wake.id);
+
+  const space = slaveResult.hold + ":" + slaveResult.sector;
+
+  const warpNormal = GALAXY_WARP.run(space, "normal");
+  const warpWorm = GALAXY_WARP.run(space, "worm");
+
+  return {
+    master: masterBlock,
+    slave: slaveResult,
+    warp: {
+      normal: warpNormal,
+      worm: warpWorm
+    }
+  };
+}
 
