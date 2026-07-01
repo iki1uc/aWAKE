27cac7da8a4059ec7eece53e52903f90f3774723
iki1uc <entertrainer@web.de>
2026-07-01 21:09:09 +0200

    Update galaxy‑pipeline.js

--- galaxy‑pipeline.js
+++ galaxy‑pipeline.js
@@ -1,1 +1,18 @@
+import { SLAVE } from "./slave.js";
+import { GALAXY_SLAVE } from "./GALAXY_SLAVE.js";
+
+export function GALAXY_PIPE(masterBlock) {
+
+  const slaveResult = SLAVE.run(masterBlock.wake.id);
+
+  const galaxyInput = slaveResult.scan + ":" + slaveResult.tick;
+
+  const galaxyResult = GALAXY_SLAVE.run(galaxyInput);
+
+  return {
+    master: masterBlock,
+    slave: slaveResult,
+    galaxy: galaxyResult
+  };
+}
 
