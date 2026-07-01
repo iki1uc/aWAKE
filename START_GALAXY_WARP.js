286041f2a503ba8cbbf91b0cf37fffe666b95a2b
iki1uc <entertrainer@web.de>
2026-07-01 21:13:26 +0200

    Update START_GALAXY_WARP.js

--- START_GALAXY_WARP.js
+++ START_GALAXY_WARP.js
@@ -1,1 +1,8 @@
+import { START_GALAXY } from "./START_GALAXY.js";
+import { GALAXY_WARP_PIPE } from "./galaxy-warp-pipeline.js";
+
+export function START_GALAXY_WARP() {
+  const galaxyBlock = START_GALAXY();
+  return GALAXY_WARP_PIPE(galaxyBlock);
+}
 
