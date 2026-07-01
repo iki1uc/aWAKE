3c36bdfce91d0182ea466133cf7ede331413d72e
iki1uc <entertrainer@web.de>
2026-07-01 21:11:53 +0200

    Update galaxy‑warp.js

--- galaxy‑warp.js
+++ galaxy‑warp.js
@@ -1,1 +1,33 @@
+export const GALAXY_WARP = {
+  id: "GALAXY_WARP",
+  warpCount: 0,
+
+  normal(space) {
+    return `NORMAL_GATE(${space})`;
+  },
+
+  worm(space) {
+    return `WURMLOCH_GATE(${space})`;
+  },
+
+  run(space, mode = "normal") {
+    this.warpCount++;
+
+    if (mode === "worm") {
+      return {
+        id: this.id,
+        warp: this.warpCount,
+        gate: this.worm(space),
+        mode: "WURMLOCH"
+      };
+    }
+
+    return {
+      id: this.id,
+      warp: this.warpCount,
+      gate: this.normal(space),
+      mode: "NORMAL"
+    };
+  }
+};
 
