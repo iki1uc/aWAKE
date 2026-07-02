bfce4b7765194617edea3817bc4dc958b26ff14e
iki1uc <entertrainer@web.de>
2026-07-02 21:52:02 +0200

    Update DEMN.js

--- DEMN.js
+++ DEMN.js
@@ -1,13 +1,6 @@
-import { START } from "./start.js";
-import { RAWATOR } from "./rawator.js";
-import { aWAKE } from "./aWAKE.js";
-import { SLAVE_PIPELINE } from "./slave-pipeline.js";
-import { GALAXY_SLAVE } from "./GALAXY-SLAVE.js";
-import { GALAXY_WARP_PIPE } from "./galaxy-warp-pipeline.js";
-import { WURMLOCH_CORE_PIPE } from "./wurmloch-core-pipeline.js";
+import { TMP } from "./SUB/tmp.js";
 
 export function DEMN() {
-
   const master = START();
   const raw = RAWATOR();
   const awake = aWAKE.run();
@@ -15,17 +8,16 @@ export function DEMN() {
   const galaxySlave = GALAXY_SLAVE.run(master.wake.id);
   const galaxyWarp = GALAXY_WARP_PIPE(master);
   const wurm = WURMLOCH_CORE_PIPE();
-
-  const vec92 = 92;
+  const tmp = TMP(awake, raw);
 
   return {
-    vec92,
     master,
     raw,
     awake,
     slave,
     galaxySlave,
     galaxyWarp,
-    wurm
+    wurm,
+    tmp
   };
 }
