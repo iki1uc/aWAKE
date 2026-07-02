6ce2bb318c2dd9ed13f57fff1e656b9aec8e5238
iki1uc <entertrainer@web.de>
2026-07-02 21:51:40 +0200

    Update tmp.js

--- SUB/tmp.js
+++ SUB/tmp.js
@@ -1,1 +1,24 @@
+import { TMP } from "./SUB/tmp.js";
+
+export function DEMN() {
+  const master = START();
+  const raw = RAWATOR();
+  const awake = aWAKE.run();
+  const slave = SLAVE_PIPELINE(master);
+  const galaxySlave = GALAXY_SLAVE.run(master.wake.id);
+  const galaxyWarp = GALAXY_WARP_PIPE(master);
+  const wurm = WURMLOCH_CORE_PIPE();
+  const tmp = TMP(awake, raw);
+
+  return {
+    master,
+    raw,
+    awake,
+    slave,
+    galaxySlave,
+    galaxyWarp,
+    wurm,
+    tmp
+  };
+}
 
